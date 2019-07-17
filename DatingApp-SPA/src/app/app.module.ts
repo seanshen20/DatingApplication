a8d31311c76abb46c7fada7433825cd864f70964
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2019-07-17 15:14:52 +1000

    end of Section 13 Action filter

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -32,7 +32,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
-
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 export const tokenGetter = () => localStorage.getItem('token');
 
@@ -48,7 +48,8 @@ export const tokenGetter = () => localStorage.getItem('token');
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
